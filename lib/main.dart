3466d0f34662cdc0483d01521a839183b3ca1045
Geoff Register <geoff.register@smashingboxes.com>
2020-09-25 16:57:23 -0400

    Tutorial: 3:09:26

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,34 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\s your tea?',
+      'What\'s my dimsum?',
+      'Corn tips',
+      'Chicken strips'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Gucci App'),
         ),
-        body: Text('This is the body text, yo.'),
+        body: Column(
+          children: [
+            Text('The Question'),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: null,
+            ),
+          ],
+        ),
       ),
     );
   }
